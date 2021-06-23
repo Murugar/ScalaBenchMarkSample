87fe2684ee63c9507ab551bd41b692c1df0eebac
Muruga <davanon2014@gmail.com>
2021-06-23 10:36:14 +0530

    First

---
+++ src/main/scala/com/iqmsoft/MyBenchmark.scala
@@ -0,0 +1,13 @@
+package com.iqmsoft
+
+import org.openjdk.jmh.annotations.Benchmark
+
+class MyBenchmark {
+
+  @Benchmark
+  def testMethod(): Any = {
+    // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+    // Put your benchmark code here.
+  }
+
+}
